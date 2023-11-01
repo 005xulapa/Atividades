32e51f684f5080b0d0230b751ba2a68db0e5b2be
005xulapa <005xulapa@gmail.com>
2023-11-01 17:15:13 -0300

    primeiro commit

---
+++ Atividades/Idade/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano_atual = DateTime.Now.Year;
+            int mes_atual = DateTime.Now.Month;
+            int dia_atual = DateTime.Now.Day;
+            int idade = 0;
+
+
+            Console.WriteLine("Qual dia voce nasceu?");
+            int dia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual mes voce nasceu?");
+            int mes = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual foi o ano do seu nascimento?");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            if (ano <= ano_atual)
+            {
+                if (mes < mes_atual)
+                {
+                    idade = ano_atual - ano;
+                    Console.WriteLine(idade);
+                }
+                else if (mes == mes_atual && dia <= dia_atual) 
+                {
+                    idade = ano_atual - ano;
+                    Console.WriteLine(idade);
+                }
+                else
+                {
+                    idade = ano_atual - ano - 1;
+                    Console.WriteLine(idade);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ano invalido");
+            }
+            
+            Console.WriteLine(ano_atual);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Atividades/Temperatura/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal temp_final = 0;
+
+            Console.WriteLine("Qual a escala de origem (1-Celsius, 2-Fahrenheit)?");
+            int origem = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual a temperatura de origem?");
+            decimal temp_origem = Convert.ToDecimal(Console.ReadLine());
+
+            if (origem == 1) 
+            {
+                temp_final = (temp_origem * 9 / 5) + 32;
+            }
+            else 
+            {
+                temp_origem = temp_origem - 32;
+                temp_final = temp_origem * 5 / 9;
+            }
+
+            Console.WriteLine(temp_final);
+
+            Console.ReadKey();
+        }
+    }
+}
